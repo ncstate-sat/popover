9a3ea53f1628503d9454fe8b4ba4151dd94044b9
Will Howell <will.s.howell@gmail.com>
2017-09-18 14:33:00 -0400

    Correct sourcemaps for umd bundle

--- tools/minify-sources.js
+++ tools/minify-sources.js
@@ -1,11 +1,29 @@
 const uglify = require('uglify-js');
 const { readFileSync, writeFileSync } = require('fs-extra');
 
-/** Function to minify a file. */
+const FILENAME_REGEX = /[^\/]+$/;
+
+/** Function to minify a file. Copy over sourcemaps as well. */
 function uglifyFile(inputPath, outputPath) {
-  const code = readFileSync(inputPath, 'utf8');
-  const result = uglify.minify(code);
+  const sourcemapIn = `${inputPath}.map`;
+  const sourcemapOut = `${outputPath}.map`;
+
+  const outputFilename = outputPath.match(FILENAME_REGEX)[0];
+  const outputSourcemapFilename = `${outputFilename}.map`;
+
+  const result = uglify.minify(
+    readFileSync(inputPath, 'utf8'),
+    {
+      sourceMap: {
+        content: readFileSync(sourcemapIn, 'utf8'),
+        filename: outputFilename,
+        url: outputSourcemapFilename,
+      }
+    }
+  );
+
   writeFileSync(outputPath, result.code);
+  writeFileSync(sourcemapOut, result.map);
 }
 
 module.exports = uglifyFile;
