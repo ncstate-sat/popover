083aa6ed3bf5562b2286024cc4ba3e5f00f7466a
Will Howell <will.s.howell@gmail.com>
2017-10-13 12:22:59 -0400

    Run karma via npm script

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,14 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+    ],
+    files: [
+      '.ng_build/spec/**/*'
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -18,9 +19,6 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
--- tools/test.js
+++ tools/test.js
@@ -1,1 +1,7 @@
-console.log('testing not yet configured')
+const path = require('path');
+const karma = require('karma');
+
+new karma.Server({
+  configFile: path.join(__dirname, '../karma.conf.js'),
+  singleRun: true,
+}).start();
