88c3c9424066cb9dd51ce9b0bfbaf59bbbded020
Will Howell <will.s.howell@gmail.com>
2017-10-16 14:10:43 -0400

    Get basic version of test working by copying over quickstart karma configs

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,8 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  var libBase = 'src/lib/';       // transpiled app JS and map files
+
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
@@ -10,16 +12,60 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
     ],
-    files: [
-      '.ng_build/spec/**/*'
-    ],
     client:{
+      builtPaths: [libBase], // add more spec base paths as needed
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+
+    files: [
+      // System.js for module loading
+      'node_modules/systemjs/dist/system.src.js',
+
+      // Polyfills
+      'node_modules/core-js/client/shim.js',
+
+      // zone.js
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
+      'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/fake-async-test.js',
+
+      // RxJs
+      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
+
+      // Paths loaded via module imports:
+      // Angular itself
+      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
+      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
+
+      { pattern: 'src/demo/systemjs-angular-loader.js', included: false, watched: false },
+
+      'karma-test-shim.js', // optionally extend SystemJS mapping e.g., with barrels
+
+      // transpiled application & spec code paths loaded via module imports
+      { pattern: libBase + '**/*.js', included: false, watched: true },
+
+      // Asset (HTML & CSS) paths loaded via Angular's component compiler
+      // (these paths need to be rewritten, see proxies section)
+      { pattern: libBase + '**/*.html', included: false, watched: true },
+      { pattern: libBase + '**/*.css', included: false, watched: true },
+
+      // Paths for debugging with source maps in dev tools
+      { pattern: libBase + '**/*.ts', included: false, watched: false },
+      { pattern: libBase + '**/*.js.map', included: false, watched: false }
+    ],
+
+    // Proxied base paths for loading assets
+    proxies: {
+      // required for modules fetched by SystemJS
+      '/base/src/lib/node_modules/': '/base/node_modules/',
+      '/base/src/lib/demo/': '/base/src/demo/'
     },
+
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
--- src/lib/popover/popover.spec.ts
+++ src/lib/popover/popover.spec.ts
@@ -1,9 +1,14 @@
+import { getInvalidPopoverError } from './popover.errors';
+
 describe('sample', () => {
 
   it('should work', () => {
     expect(true).toBe(true);
+  });
+
+  it('should return an Error', () => {
+    const result = getInvalidPopoverError();
+    expect(result instanceof Error).toBeTruthy();
   })
 
 });
-
-console.log('test!')
