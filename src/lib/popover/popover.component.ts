d174cde56def8938d6dc94072e00bcbb17861c15
Will Howell <will.s.howell@gmail.com>
2017-10-18 17:06:40 -0400

    Address self review

--- src/lib/popover/popover.component.ts
+++ src/lib/popover/popover.component.ts
@@ -29,6 +29,7 @@ import { getUnanchoredPopoverError } from './popover.errors';
 export type SatPopoverPositionX = 'before' | 'center' | 'after';
 export type SatPopoverPositionY = 'above'  | 'center' | 'below';
 
+// See http://cubic-bezier.com/#.25,.8,.25,1 for reference.
 const OPEN_TRANSITION  = '200ms cubic-bezier(0.25, 0.8, 0.25, 1)';
 const CLOSE_TRANSITION = OPEN_TRANSITION;
 
