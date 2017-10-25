65220da2d62736b3d410c9704c141cf456201424
Will Howell <will.s.howell@gmail.com>
2017-10-25 11:26:34 -0400

    Followup - remove bidi module import

--- src/demo/app/demo.module.ts
+++ src/demo/app/demo.module.ts
@@ -14,7 +14,6 @@ import {
   MatDatepickerModule,
   MatNativeDateModule,
 } from '@angular/material';
-import { BidiModule } from '@angular/cdk/bidi';
 
 import { DemoComponent } from './demo.component';
 import { PositioningDemo } from './positioning/positioning.component';
@@ -27,7 +26,6 @@ import { TooltipDemo } from './tooltip/tooltip.component';
 
 @NgModule({
   exports: [
-    // material
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
@@ -37,8 +35,6 @@ import { TooltipDemo } from './tooltip/tooltip.component';
     MatCheckboxModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    // cdk
-    BidiModule,
   ]
 })
 export class DemoMaterialModule { }
