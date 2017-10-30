8103cb80a1c3c445a53c3adc4a6ce3697067cb31
Will Howell <will.s.howell@gmail.com>
2017-10-30 16:39:38 -0400

    Mess around with rollup globals trying to get a build

--- tools/utils/rollup-globals.js
+++ tools/utils/rollup-globals.js
@@ -12,13 +12,51 @@ const GLOBALS = {
   '@angular/cdk/overlay': 'ng.cdk.overlay',
   '@angular/cdk/portal': 'ng.cdk.portal',
 
-  // rxjs globals
-  'rxjs/Subject': 'Rx',
+
+
+  // 'rxjs/observable/merge': 'Rx.Observable',
+  // 'rxjs/operators': 'Rx.Observable',
+  // 'rxjs/operators/index': 'Rx.Observable',
+
+  // // rxjs globals
+  // 'rxjs/Subject': 'Rx',
+  // 'rxjs/Observable': 'Rx',
+  // 'rxjs/add/operator/takeUntil': 'Rx.Observable.prototype',
+  // 'rxjs/add/operator/switchMap': 'Rx.Observable.prototype',
+  // 'rxjs/add/operator/take': 'Rx.Observable.prototype',
+  // 'rxjs/add/observable/merge': 'Rx.Observable',
+
+  'rxjs/BehaviorSubject': 'Rx',
   'rxjs/Observable': 'Rx',
-  'rxjs/add/operator/takeUntil': 'Rx.Observable.prototype',
-  'rxjs/add/operator/switchMap': 'Rx.Observable.prototype',
-  'rxjs/add/operator/take': 'Rx.Observable.prototype',
+  'rxjs/Subject': 'Rx',
+  'rxjs/Subscription': 'Rx',
+  'rxjs/Observer': 'Rx',
+  'rxjs/Subscriber': 'Rx',
+  'rxjs/Scheduler': 'Rx',
+  'rxjs/observable/combineLatest': 'Rx.Observable',
+  'rxjs/observable/forkJoin': 'Rx.Observable',
+  'rxjs/observable/fromEvent': 'Rx.Observable',
+  'rxjs/observable/merge': 'Rx.Observable',
+  'rxjs/observable/of': 'Rx.Observable',
+  'rxjs/observable/throw': 'Rx.Observable',
+  'rxjs/observable/defer': 'Rx.Observable',
+  'rxjs/observable/fromEventPattern': 'Rx.Observable',
+  'rxjs/observable/empty': 'Rx.Observable',
+  'rxjs/operators': 'Rx.Observable',
+  'rxjs/operators/index': 'Rx.Observable',
+
   'rxjs/add/observable/merge': 'Rx.Observable',
+  'rxjs/add/observable/fromEvent': 'Rx.Observable',
+  'rxjs/add/observable/of': 'Rx.Observable',
+  'rxjs/add/observable/interval': 'Rx.Observable',
+  'rxjs/add/operator/startWith': 'Rx.Observable.prototype',
+  'rxjs/add/operator/map': 'Rx.Observable.prototype',
+  'rxjs/add/operator/debounceTime': 'Rx.Observable.prototype',
+  'rxjs/add/operator/distinctUntilChanged': 'Rx.Observable.prototype',
+  'rxjs/add/operator/first': 'Rx.Observable.prototype',
+  'rxjs/add/operator/catch': 'Rx.Observable.prototype',
+  'rxjs/add/operator/switchMap': 'Rx.Observable.prototype'
+
 };
 
 module.exports = GLOBALS;
