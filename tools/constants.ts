706443f5ef8f64c8a1ab914cf3edf9b3765b77f4
Will Howell <will.s.howell@gmail.com>
2018-05-30 16:20:35 -0400

    fix: copy readme to dist so it shows up on npm (#128)
    * chore: copy readme to dist so it lands on npm
    
    * Add 'popper' to npm keywords
    
    * fix lint failures

--- tools/constants.ts
+++ tools/constants.ts
@@ -2,8 +2,10 @@ import { join } from 'path';
 
 // paths
 export const SOURCE_PACKAGE_PATH = join(__dirname, '..', 'package.json');
+export const SOURCE_README_PATH = join(__dirname, '..', 'README.md');
 export const DIST_PATH = join(__dirname, '..', 'dist', 'popover');
 export const DIST_PACKAGE_PATH = join(DIST_PATH, 'package.json');
+export const DIST_README_PATH = join(DIST_PATH, 'README.md');
 
 // config
 export const PEER_DEPENDENCIES = ['@angular/common', '@angular/core', '@angular/cdk'];
