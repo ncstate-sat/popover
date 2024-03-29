4c7c670af43504d9edcdabb09a5e22b9df15c31d
Will Jaspers <wjaspers@users.noreply.github.com>
2024-03-29 11:18:16 -0500

    Switch to picocolors, update @angular/cdk and @angular/material
    Discovered this little gem while skimming lockfile changes in other
    projects. No need to override the `moduleResolution` anymore, and no
    dynamic imports required.
    
    Upgrade @angular/cdk and @angular/material

--- tools/release.ts
+++ tools/release.ts
@@ -1,39 +1,38 @@
-const dynamicImportChalk = import('chalk');
-let chalk: any;
 import * as childProcess from 'child_process';
 import * as util from 'util';
+import pc from 'picocolors';
 import { readFileSync } from 'fs';
 import { DIST_PATH, DIST_PACKAGE_PATH } from './constants';
 const exec = util.promisify(childProcess.exec);
 
 async function gitTags() {
   const { version } = JSON.parse(readFileSync(DIST_PACKAGE_PATH, 'utf8'));
-  console.log(chalk.green(`Tagging with ${version}`));
+  console.log(pc.green(`Tagging with ${version}`));
 
   const { stdout: tag } = await exec(`git tag v${version}`);
   const { stdout: push } = await exec(`git push --tags`);
 
   if (tag) {
-    console.log(chalk.gray(tag));
+    console.log(pc.gray(tag));
   }
 
   if (push) {
-    console.log(chalk.gray(push));
+    console.log(pc.gray(push));
   }
 }
 
 async function libBuild() {
-  console.log(chalk.green(`Building library`));
+  console.log(pc.green(`Building library`));
 
   const { stdout: build } = await exec(`npm run build:prod`);
 }
 
 async function publish() {
-  console.log(chalk.green(`Publishing`));
+  console.log(pc.green(`Publishing`));
 
   const { stdout } = await exec(`npm publish ${DIST_PATH}`);
 
-  console.log(chalk.gray(stdout));
+  console.log(pc.gray(stdout));
 }
 
 async function release() {
@@ -42,7 +41,4 @@ async function release() {
   await publish();
 }
 
-dynamicImportChalk.then((chalkModule) => {
-  chalk = new chalkModule.Chalk();
-  release();
-});
+release();
