6fe90cd6de43f89dda98045fcf2c82e2ed36af42
Isaac Kehle <ikehle@pm.me>
2026-05-13 15:52:23 -0400

    feat(public): add and prepare index.html for deployment
    Add a new `public/index.html` file and update the preparation script to include it in the distribution.

--- tools/constants.ts
+++ tools/constants.ts
@@ -3,9 +3,11 @@ import { join } from 'path';
 // paths
 export const SOURCE_PACKAGE_PATH = join(__dirname, '..', 'package.json');
 export const SOURCE_README_PATH = join(__dirname, '..', 'README.md');
+export const SOURCE_INDEX_PATH = join(__dirname, '..', 'public', 'index.html');
 export const DIST_PATH = join(__dirname, '..', 'dist', 'popover');
 export const DIST_PACKAGE_PATH = join(DIST_PATH, 'package.json');
 export const DIST_README_PATH = join(DIST_PATH, 'README.md');
+export const DIST_INDEX_PATH = join(DIST_PATH,   'index.html');
 
 // config
 export const PEER_DEPENDENCIES = ['@angular/common', '@angular/core', '@angular/cdk'];
